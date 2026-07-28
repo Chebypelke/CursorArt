d1400e1a102d1048e691491608b1acc33505b470
Chebypelke <andrey.phonk@gmail.com>
2026-07-28 23:20:54 +0400

    chore: remove debug code from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,11 @@
-#include "CursorArt.hpp"
 #include <Arduino.h>
-#include <Mouse.h>
 
 // Nothing...
 
 void setup() 
 {
-    delay(5000);
-    Mouse.begin();
 }
 
 void loop() 
 {
-    CursorArt::roseCurve(10.0f, 5);
 }
