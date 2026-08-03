46cb764ca1c5850b6a41701bfcc9876363ce7ef6
Chebypelke <andrey.phonk@gmail.com>
2026-08-03 20:58:06 +0400

    docs: add project header comment to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <Arduino.h>
 
-// Nothing...
+/*
+    CursorArt - a lightweight C++ library for drawing mathematical cursor trajectories on USB HID capable Arduino boards.
+    License - MIT
+    Author - Chebypela( https://github.com/Chebypelke )
+*/
 
 void setup() 
 {
