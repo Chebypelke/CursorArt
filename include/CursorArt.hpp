9fa56b94a6bf6a50b8b9020ef7f602322038dec8
Chebypelke <andrey.phonk@gmail.com>
2026-08-06 18:08:54 +0400

    docs: add comments to CursorArt.hpp

--- include/CursorArt.hpp
+++ include/CursorArt.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+/*
+    The main include file for using the library.
+
+    CursorArt - a lightweight C++ library for drawing mathematical cursor trajectories on USB HID capable Arduino boards.
+    License - MIT
+    Author - Chebypela( https://github.com/Chebypelke )
+*/
+
 namespace CursorArt {
     void ellipse(int width, int height);
     void spiral(float scale, int turns);
